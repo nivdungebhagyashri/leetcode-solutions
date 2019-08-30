9e161c7d93b84f1031fe22e09fa99a933d63a941
nivdungebhagyashri <vibha.nivdunge@gmail.com>
2019-08-30 14:20:18 -0700

    codes

--- 3sumClosest.js
+++ 3sumClosest.js
@@ -6,20 +6,32 @@
 
 The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).*/
 
+
 var threeSumClosest = function(nums, target) {
-    // let sum=0;
-    nums = nums.sort((a,b) => a - b);
-    for (let i=0;i<=nums.length-2;i++){
-        
-        for(let j = i + 1, k = nums.length - 1; j < k;){
-          let sum= nums[i]+nums[j]+sums[k]
-          let dif=target-sum;
-        }
-        if(diff===0)return sum;
-        else{
+    let nearestSum='';
+    nums = nums.sort((a,b)=>a-b);
+    console.log(nums);
+    for(let i=0;i<nums.length-2;i++){
+        for(let j=i+1,k=nums.length-1;j<k;){
+            let sum = nums[i]+nums[j]+nums[k];
+            let dif = sum-target;
+            console.log(sum);
             
+            if(dif===0)return sum;
+            else{
+                if(nearestSum===''|| Math.abs(dif)<Math.abs(nearestSum-target)){
+                    nearestSum=sum;
+                    console.log("here");
+                }
+                if(dif>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
         }
     }
     
-    
+    return nearestSum
 };
