f4be95370153bc21d2eb847af345d6756999df75
nivdungebhagyashri <vibha.nivdunge@gmail.com>
2019-11-16 20:56:40 -0800

    Update linked_list_middle_node.js

--- linked_list_middle_node.js
+++ linked_list_middle_node.js
@@ -1,12 +1,10 @@
 var middleNode = function(head) {
     let slow = head;
     let fast = head;
-    while(fast.next!==null && fast.next.next!==null ){
+    while(fast!==null && fast.next!==null ){
        fast=fast.next.next;
        slow=slow.next;
     }
-    if(fast.next != null){
-        slow = slow.next;
-    }
+    
     return slow;
 };
